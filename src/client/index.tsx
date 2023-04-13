895dd4beb67d04e152b8b0f6cde1c3ccd7bde23a
ymkz <ymkz@users.noreply.github.com>
2023-04-13 17:35:39 +0000

    chore: fix unformatted import

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,11 +1,10 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Container, CssBaseline } from './components/mui-material'
 import { NovelForm } from './components/novel-form'
 import { NovelList } from './components/novel-list'
 import { NovelViewer } from './components/novel-viewer'
-
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 const queryClient = new QueryClient()
 
