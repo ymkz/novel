c9050d314c11ab1a8cc93fc6799eae8fae271be3
ymkz <ymkz@users.noreply.github.com>
2023-10-25 00:40:32 +0900

    style: anchor item element

--- src/application/component/novel-item.tsx
+++ src/application/component/novel-item.tsx
@@ -12,16 +12,19 @@ type Props = {
 export const NovelItem = ({ narouNovel }: Props) => {
   return (
     <li class="item">
-      <a href={generateNarouHref(narouNovel.ncode, narouNovel.currentPage)}>
-        <div class="title">{narouNovel.title}</div>
-        <div class="info">
-          <p class="page">
-            {generatePageInfo(narouNovel.currentPage, narouNovel.totalPage)}
-          </p>
-          <p class="lastPublishedAt">{narouNovel.lastPublishedAt}</p>
-          <p class="url">{generateNcodeCaption(narouNovel.ncode)}</p>
-        </div>
-      </a>
+      {/* biome-ignore lint/a11y/useAnchorContent: <explanation> */}
+      <a
+        class="link"
+        href={generateNarouHref(narouNovel.ncode, narouNovel.currentPage)}
+      />
+      <div class="title">{narouNovel.title}</div>
+      <div class="info">
+        <p class="page">
+          {generatePageInfo(narouNovel.currentPage, narouNovel.totalPage)}
+        </p>
+        <p class="lastPublishedAt">{narouNovel.lastPublishedAt}</p>
+        <p class="url">{generateNcodeCaption(narouNovel.ncode)}</p>
+      </div>
     </li>
   );
 };
