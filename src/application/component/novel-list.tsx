a4ccbe4b30c14f3a275845d68880c6d04c9ea85c
ymkz <ymkz@users.noreply.github.com>
2024-01-07 00:13:29 +0900

    style: modify app color

--- src/application/component/novel-list.tsx
+++ src/application/component/novel-list.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export const NovelList = ({ narouNovelList }: Props) => {
   return (
-    <main id="main" class="p:8px|16px|32px">
+    <main class="p:8px|16px|32px">
       <ul class="flex flex-direction:column gap:24px">
         {narouNovelList.map((narouNovel) => (
           <NovelItem narouNovel={narouNovel} />
